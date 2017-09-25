04b5ddd745ceb9bbfc26df68ccb68cf80dd8253c
Chris Hambleton <christopher_hambleton@trimble.com>
2017-09-25 17:36:08 -0600

    adding materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { HttpModule } from '@angular/http';
 import { ChartLyricsService } from './services/chart-lyrics.service';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {MdButtonModule, MdCheckboxModule, MdToolbarModule, MdMenuModule, MdIconModule} from '@angular/material';
 import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +19,9 @@ import { ModalModule } from './dashboard/modal/modal.module';
   ],
   imports: [    
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
+    MdButtonModule, MdCheckboxModule, MdToolbarModule, MdMenuModule, MdIconModule,
     HttpModule,
     ModalModule,
     AppRoutingModule,
